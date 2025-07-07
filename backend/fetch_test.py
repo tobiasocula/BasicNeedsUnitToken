de78b190bfdd4b4f6590a9dc46699b7e08def6e8
Tobias Ocula <tobias.ocula@outlook.com>
2025-07-07 10:52:17 +0200

    removed venv from repo

---
+++ backend/fetch_test.py
@@ -0,0 +1,6 @@
+import requests
+
+response = requests.post('https://basic-needs-unit-token-zc3p.vercel.app/test',
+                         json={'msg': 'hello!'})
+print(response.status_code)
+print(response.json())
